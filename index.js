1a51d9d07316850a7d1870b31656beaf0ce983ab
eduviictor <eduvictornobrega@gmail.com>
2020-05-25 20:43:37 -0300

    add isCompressed method and types

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const audios = require("./audios.json");
 const images = require("./images.json");
+const compresseds = require("./compresseds.json");
 
 const isAudio = (name) => {
   const audiosExt = new Set(audios);
@@ -10,6 +11,11 @@ const isAudio = (name) => {
 const isImage = (name) => {
   const imagesExt = new Set(images);
   return imagesExt.has(path.extname(name).slice(1));
+};
+
+const isCompressed = (name) => {
+  const compressedsExt = new Set(compresseds);
+  return compressedsExt.has(path.extname(name).slice(1));
 };
 
 module.exports = {
