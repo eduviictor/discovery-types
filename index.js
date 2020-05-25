172e0934830276886a543c0af16c3f05019d578c
eduviictor <eduvictornobrega@gmail.com>
2020-05-25 20:43:11 -0300

    add isImage method and types

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const path = require("path");
 const audios = require("./audios.json");
+const images = require("./images.json");
 
 const isAudio = (name) => {
   const audiosExt = new Set(audios);
   return audiosExt.has(path.extname(name).slice(1));
+};
+
+const isImage = (name) => {
+  const imagesExt = new Set(images);
+  return imagesExt.has(path.extname(name).slice(1));
 };
 
 module.exports = {
