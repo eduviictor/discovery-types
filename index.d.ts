43903721a4c6fe6bb05c7d0b19992a556b6672d6
eduviictor <eduvictornobrega@gmail.com>
2020-05-25 21:16:21 -0300

    add types

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+declare function isAudio(name: string): boolean;
+
+declare function isImage(name: string): boolean;
+
+declare function isCompressed(name: string): boolean;
+
+declare function isExecutable(name: string): boolean;
+
+declare function isFont(name: string): boolean;
+
+declare function isVideo(name: string): boolean;
+
+declare function isDocument(name: string): boolean;
+
+declare function verify(name: string): boolean;
